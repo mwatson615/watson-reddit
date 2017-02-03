f89da57d4bd23b6d701d39cf62239b86181f3516
Michael Watson <m.watson.tn@gmail.com>
2017-02-02 19:13:58 -0600

    fb updating correctly, got email and img url

--- controllers/create.js
+++ controllers/create.js
@@ -2,13 +2,46 @@
 
 app.controller('CreateCtrl', function($scope, createFactory, authFactory) {
 
+		//create route storage reference
+	let storageRef = firebase.storage().ref();
+
+	let inputElement = document.getElementById("fileInput")
+
+	let imgArray = []
+
+	let imgUrl = ""
+
+
+	inputElement.addEventListener('change', handleFiles, false);
+
+	function handleFiles() {
+		let fileList = this.files;
+     /* now you can work with the file list */
+    console.log("filelist", fileList)
+    storageRef.child(fileList[0].name).put(fileList[0])
+      .then(function(snapshot) {
+        console.log('Uploaded a blob or file!');
+      });
+
+    storageRef.child('brasstownfalls.jpg').getDownloadURL()
+	.then(function(url) {
+      // var img = document.getElementById('myImg');
+      // img.src = url;
+      // console.log(img)
+      imgUrl = url;
+      console.log(imgUrl)
+      $scope.images = fileList;
+    }).catch(function(error) {
+    // Handle any errors
+  })
+}
 	$scope.currentUser = authFactory.getUserId()
-	console.log($scope.currentUser.$$state.value)
+	// console.log($scope.currentUser.$$state.value)
 	$scope.createPost = () => {
 	let newPost = {
 		"title" : $scope.title,
 		"url" : $scope.url,
-		"imageName" : null,
+		"imgUrl" : imgUrl,
 		"counter" : 0,
 		"userEmail" : $scope.currentUser.$$state.value.email
 	}
@@ -18,36 +51,6 @@ app.controller('CreateCtrl', function($scope, createFactory, authFactory) {
 
 
 
-	//create route storage reference
-	let storageRef = firebase.storage().ref();
 
-	let inputElement = document.getElementById("fileInput")
-
-	let imgArray = []
-
-
-	inputElement.addEventListener('change', handleFiles, false);
-
-	function handleFiles() {
-		let fileList = this.files;
-     /* now you can work with the file list */
-    // console.log("filelist", fileList)
-    storageRef.child(fileList[0].name).put(fileList[0])
-      .then(function(snapshot) {
-        console.log('Uploaded a blob or file!');
-      });
-
-  //getting the url
-  storageRef.child(this.files).getDownloadURL()
-	.then(function(url) {
-      var img = document.getElementById('myImg');
-      img.src = url;
-      console.log(img)
-      newPost.uploadUrl = url;
-      $scope.images = fileList;
-    }).catch(function(error) {
-    // Handle any errors
-  })
-}
 
 })
